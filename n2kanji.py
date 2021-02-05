5781423ffdc2dd9abe578ad5d7aaedbc6cece810
anjiboss <nhu.nhu.face@gmail.com>
2021-02-05 10:50:35 +0900

    craped N2

---
+++ n2kanji.py
@@ -0,0 +1,18 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+fileName = "n2webcrap.csv"
+header = "kanji, hanViet\n"
+f = open(fileName, "w",  encoding="utf-8")
+f.write(header)
+
+url = "https://quizlet.com/jp/564323390/toan-bo-328-han-tu-2k-mina-no-kanji-flash-cards/"
+req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+web_byte = urlopen(req).read()
+page_html = web_byte.decode("utf-8")
+page_soup = soup(page_html, "html.parser")
+containers = page_soup.findAll("div", {"class":"SetPageTerm-content"})
+for container in containers:
+	kanji = container.a.text
+	hanViet =  container.findAll("span", {"class": "TermText notranslate lang-vi"})[0].text
+	f.write(kanji + "    ,     " + hanViet+ "   \n")
